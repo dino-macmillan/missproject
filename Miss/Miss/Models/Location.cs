00f7c17d53d84e92c8578812ab2d37484f06dca6
Mohammad Salman <msalman@macmillan.org.uk>
2017-06-29 09:37:23 +0100

    Added DataService

---
+++ Miss/Miss/Models/Location.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Miss.Models
+{
+    public class Location
+    {
+        public string Date { get; set; }
+        public string Month { get; set; }
+        public string Year { get; set; }
+        public string Address { get; set; }
+        public string  Postcode { get; set; }
+        public double Lat { get; set; }
+        public double Long { get; set; }
+
+    }
+}
